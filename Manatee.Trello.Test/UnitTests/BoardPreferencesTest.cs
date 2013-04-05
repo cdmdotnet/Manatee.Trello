e4ab3c657d85ae394d9a57fffe11b46a0bde8c10
gdennis <gregsdennis@users.noreply.github.com>
2013-04-04 21:50:39 -0500

    Completed unit tests for Board and BoardPreferences. Need to implement negative testing for Action, Attachment, BoardMembership, and BoardPersonalPreferences. Fixed a few bugs.

--- Manatee.Trello.Test/UnitTests/BoardPreferencesTest.cs
+++ Manatee.Trello.Test/UnitTests/BoardPreferencesTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoryQ;
 
 namespace Manatee.Trello.Test.UnitTests
 {
@@ -9,39 +10,195 @@ namespace Manatee.Trello.Test.UnitTests
 		[TestMethod]
 		public void AllowsSelfJoin()
 		{
-			throw new NotImplementedException();
+			var story = new Story("AllowsSelfJoin");
+
+			var feature = story.InOrderTo("control whether the board allows members to join without an invitation")
+				.AsA("developer")
+				.IWant("to get the AllowsSelfJoin property value.");
+
+			feature.WithScenario("Access AllowsSelfJoin property")
+				.Given(ABoardPreferencesObject)
+				.And(EntityIsExpired)
+				.When(AllowsSelfJoinIsAccessed)
+				.Then(MockApiGetIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set AllowsSelfJoin property")
+				.Given(ABoardPreferencesObject)
+				.When(AllowsSelfJoinIsSet, (bool?) true)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set AllowsSelfJoin property to null")
+				.Given(ABoardPreferencesObject)
+				.And(AllowsSelfJoinIs, (bool?) true)
+				.When(AllowsSelfJoinIsSet, (bool?) null)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 0)
+				.And(ExceptionIsThrown<ArgumentNullException>)
+
+				.WithScenario("Set AllowsSelfJoin property to same")
+				.Given(ABoardPreferencesObject)
+				.And(AllowsSelfJoinIs, (bool?) true)
+				.When(AllowsSelfJoinIsSet, (bool?) true)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 0)
+				.And(ExceptionIsNotThrown)
+
+				.Execute();
 		}
 		[TestMethod]
 		public void Comments()
 		{
-			throw new NotImplementedException();
+			var story = new Story("Comments");
+
+			var feature = story.InOrderTo("control whom is allowed to post comments")
+				.AsA("developer")
+				.IWant("to get the Comments property value.");
+
+			feature.WithScenario("Access Comments property")
+				.Given(ABoardPreferencesObject)
+				.And(EntityIsExpired)
+				.When(CommentsIsAccessed)
+				.Then(MockApiGetIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set Comments property")
+				.Given(ABoardPreferencesObject)
+				.When(CommentsIsSet, BoardCommentType.Org)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.Execute();
 		}
 		[TestMethod]
 		public void Invitations()
 		{
-			throw new NotImplementedException();
+			var story = new Story("Invitations");
+
+			var feature = story.InOrderTo("control whom is allowed to invite members to the board")
+				.AsA("developer")
+				.IWant("to get the Invitations property value.");
+
+			feature.WithScenario("Access Invitations property")
+				.Given(ABoardPreferencesObject)
+				.And(EntityIsExpired)
+				.When(InvitationsIsAccessed)
+				.Then(MockApiGetIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set Invitations property")
+				.Given(ABoardPreferencesObject)
+				.When(InvitationsIsSet, BoardInvitationType.Members)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.Execute();
 		}
 		[TestMethod]
 		public void PermissionLevel()
 		{
-			throw new NotImplementedException();
+			var story = new Story("PermissionLevel");
+
+			var feature = story.InOrderTo("control the board's exposure")
+				.AsA("developer")
+				.IWant("to get the PermissionLevel property value.");
+
+			feature.WithScenario("Access PermissionLevel property")
+				.Given(ABoardPreferencesObject)
+				.And(EntityIsExpired)
+				.When(PermissionLevelIsAccessed)
+				.Then(MockApiGetIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set PermissionLevel property")
+				.Given(ABoardPreferencesObject)
+				.When(PermissionLevelIsSet, BoardPermissionLevelType.Public)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.Execute();
 		}
 		[TestMethod]
 		public void ShowCardCovers()
 		{
-			throw new NotImplementedException();
+			var story = new Story("ShowCardCovers");
+
+			var feature = story.InOrderTo("control whether the board shows card covers")
+				.AsA("developer")
+				.IWant("to get the ShowCardCovers property value.");
+
+			feature.WithScenario("Access ShowCardCovers property")
+				.Given(ABoardPreferencesObject)
+				.And(EntityIsExpired)
+				.When(ShowCardCoversIsAccessed)
+				.Then(MockApiGetIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set ShowCardCovers property")
+				.Given(ABoardPreferencesObject)
+				.When(ShowCardCoversIsSet, (bool?) true)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set ShowCardCovers property to null")
+				.Given(ABoardPreferencesObject)
+				.And(ShowCardCoversIs, (bool?) true)
+				.When(ShowCardCoversIsSet, (bool?) null)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 0)
+				.And(ExceptionIsThrown<ArgumentNullException>)
+
+				.WithScenario("Set ShowCardCovers property to same")
+				.Given(ABoardPreferencesObject)
+				.And(ShowCardCoversIs, (bool?) true)
+				.When(ShowCardCoversIsSet, (bool?) true)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 0)
+				.And(ExceptionIsNotThrown)
+
+				.Execute();
 		}
 		[TestMethod]
 		public void Voting()
 		{
-			throw new NotImplementedException();
+			var story = new Story("Voting");
+
+			var feature = story.InOrderTo("control whom is allowed to cast votes")
+				.AsA("developer")
+				.IWant("to get the Voting property value.");
+
+			feature.WithScenario("Access Voting property")
+				.Given(ABoardPreferencesObject)
+				.And(EntityIsExpired)
+				.When(VotingIsAccessed)
+				.Then(MockApiGetIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.WithScenario("Set Voting property")
+				.Given(ABoardPreferencesObject)
+				.When(VotingIsSet, BoardVotingType.Disabled)
+				.Then(MockApiPutIsCalled<BoardPreferences>, 1)
+				.And(ExceptionIsNotThrown)
+
+				.Execute();
 		}
 
 		#region Given
 
-		private void ABoardMembership()
+		private void ABoardPreferencesObject()
 		{
 			_systemUnderTest = new SystemUnderTest();
+			_systemUnderTest.Sut.Svc = _systemUnderTest.Dependencies.Api.Object;
+			SetupMockGet<BoardPreferences>();
+			SetupMockPut<BoardPreferences>();
+		}
+		private void AllowsSelfJoinIs(bool? value)
+		{
+			_systemUnderTest.Sut.Svc = null;
+			_systemUnderTest.Sut.AllowsSelfJoin = value;
+			_systemUnderTest.Sut.Svc = _systemUnderTest.Dependencies.Api.Object;
+		}
+		private void ShowCardCoversIs(bool? value)
+		{
+			_systemUnderTest.Sut.Svc = null;
+			_systemUnderTest.Sut.ShowCardCovers = value;
 			_systemUnderTest.Sut.Svc = _systemUnderTest.Dependencies.Api.Object;
 		}
 
@@ -49,6 +206,54 @@ namespace Manatee.Trello.Test.UnitTests
 
 		#region When
 
+		private void AllowsSelfJoinIsAccessed()
+		{
+			Execute(() => _systemUnderTest.Sut.AllowsSelfJoin);
+		}
+		private void AllowsSelfJoinIsSet(bool? value)
+		{
+			Execute(() => _systemUnderTest.Sut.AllowsSelfJoin = value);
+		}
+		private void CommentsIsAccessed()
+		{
+			Execute(() => _systemUnderTest.Sut.Comments);
+		}
+		private void CommentsIsSet(BoardCommentType value)
+		{
+			Execute(() => _systemUnderTest.Sut.Comments = value);
+		}
+		private void InvitationsIsAccessed()
+		{
+			Execute(() => _systemUnderTest.Sut.Invitations);
+		}
+		private void InvitationsIsSet(BoardInvitationType value)
+		{
+			Execute(() => _systemUnderTest.Sut.Invitations = value);
+		}
+		private void PermissionLevelIsAccessed()
+		{
+			Execute(() => _systemUnderTest.Sut.PermissionLevel);
+		}
+		private void PermissionLevelIsSet(BoardPermissionLevelType value)
+		{
+			Execute(() => _systemUnderTest.Sut.PermissionLevel = value);
+		}
+		private void ShowCardCoversIsAccessed()
+		{
+			Execute(() => _systemUnderTest.Sut.ShowCardCovers);
+		}
+		private void ShowCardCoversIsSet(bool? value)
+		{
+			Execute(() => _systemUnderTest.Sut.ShowCardCovers = value);
+		}
+		private void VotingIsAccessed()
+		{
+			Execute(() => _systemUnderTest.Sut.Voting);
+		}
+		private void VotingIsSet(BoardVotingType value)
+		{
+			Execute(() => _systemUnderTest.Sut.Voting = value);
+		}
 
 		#endregion
 	}
