c88b6d61d9b421f2429e54fe2c814e3a9c5e5c67
Greg Dennis <gregsdennis@yahoo.com>
2016-11-03 14:18:43 +1300

    Feature/mit license (#41)
    * updated license file
    
    * removed apache license callouts from manatee.trello
    
    * removed apache license callouts from manatee.trello.manateejson
    
    * removed apache license callouts from manatee.trello.restsharp and manatee.trello.webapi

--- Manatee.Trello/Json/IDeserializer.cs
+++ Manatee.Trello/Json/IDeserializer.cs
@@ -1,27 +1,4 @@
-﻿/***************************************************************************************
-
-	Copyright 2013 Little Crab Solutions
-
-	   Licensed under the Apache License, Version 2.0 (the "License");
-	   you may not use this file except in compliance with the License.
-	   You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
- 
-	File Name:		IDeserializer.cs
-	Namespace:		Manatee.Trello.Json
-	Class Name:		IDeserializer
-	Purpose:		Defines methods required by the IRestClient to deserialize a
-					response from JSON to an object.
-
-***************************************************************************************/
-using Manatee.Trello.Rest;
+﻿using Manatee.Trello.Rest;
 
 namespace Manatee.Trello.Json
 {
--- Manatee.Trello/UnknownPowerUp.cs
+++ Manatee.Trello/UnknownPowerUp.cs
@@ -2,6 +2,9 @@
 
 namespace Manatee.Trello
 {
+	/// <summary>
+	/// Provides an implementation for an unknown power-up.
+	/// </summary>
 	public class UnknownPowerUp : PowerUpBase
 	{
 		internal UnknownPowerUp(IJsonPowerUp json, TrelloAuthorization auth)
