4de103eb606cd49e3c27229c0b9b89a1d1334a16
Greg Dennis <greg.dennis@intergen.co.nz>
2016-11-03 14:10:07 +1300

    removed apache license callouts from manatee.trello.manateejson

--- Manatee.Trello.ManateeJson/Entities/ManateeNotificationData.cs
+++ Manatee.Trello.ManateeJson/Entities/ManateeNotificationData.cs
@@ -1,28 +1,4 @@
-﻿/***************************************************************************************
-
-	Copyright 2013 Little Crab Solutions
-
-	   Licensed under the Apache License, Version 2.0 (the "License");
-	   you may not use this file except in compliance with the License.
-	   You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
- 
-	File Name:		ManateeNotificationData.cs
-	Namespace:		Manatee.Trello.ManateeJson.Entities
-	Class Name:		ManateeNotificationData
-	Purpose:		Implements IJsonNotificationData for Manatee.Json.
-
-***************************************************************************************/
-
-using System;
-using Manatee.Json;
+﻿using Manatee.Json;
 using Manatee.Json.Serialization;
 using Manatee.Trello.Json;
 
