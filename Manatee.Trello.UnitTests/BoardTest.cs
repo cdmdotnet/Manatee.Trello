e0300aa8581fc1542611d24481d69fd0792f8001
Greg Dennis <gregsdennis@yahoo.com>
2017-06-26 22:47:39 +1200

    fixed tests

--- Manatee.Trello.UnitTests/BoardTest.cs
+++ Manatee.Trello.UnitTests/BoardTest.cs
@@ -11,55 +11,59 @@ namespace Manatee.Trello.UnitTests
 	[TestFixture]
 	public class BoardTest
 	{
-		private readonly MockedRestClientProvider _clientProvider = new MockedRestClientProvider();
-
-		[OneTimeSetUp]
-		public void Initialize()
+		private static MockedRestClientProvider Initialize()
 		{
+			var clientProvider = new MockedRestClientProvider();
 			TrelloConfiguration.JsonFactory = MockedJsonFactory.Instance;
-			TrelloConfiguration.RestClientProvider = _clientProvider;
+			TrelloConfiguration.RestClientProvider = clientProvider;
+			return clientProvider;
 		}
 
 		[Test]
 		public void CreationDoesNotDownloadDetails()
 		{
+			var clientProvider =  Initialize();
+
 			var board = new Board(TrelloIds.ShortFakeId);
 
-			_clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Never);
+			clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Never);
 		}
 
 		[Test]
 		public void IdAccessTriggersCall()
 		{
-			_clientProvider.ClientMock.SetupFor<IJsonBoard>();
+			var clientProvider = Initialize();
+			clientProvider.ClientMock.SetupFor<IJsonBoard>();
 
 			var board = new Board(TrelloIds.ShortFakeId);
 			var id = board.Id;
 
-			_clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Once);
+			clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Once);
 		}
 
 		[Test]
 		public void RapidIdAccessTriggersSingleCall()
 		{
+			var clientProvider = Initialize();
 			var data = new Mock<IJsonBoard>();
 			data.SetupGet(d => d.Id).Returns(TrelloIds.FakeId);
-			_clientProvider.ClientMock.SetupFor(data.Object);
+			clientProvider.ClientMock.SetupFor(data.Object);
 
 			var board = new Board(TrelloIds.ShortFakeId);
 			var id = board.Id;
 			var id2 = board.Id;
 
-			_clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Once);
+			clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Once);
 		}
 
 		[Test]
 		public void DelayedNameAccessTriggersMultipleCalls()
 		{
+			var clientProvider = Initialize();
 			var data = new Mock<IJsonBoard>();
 			data.SetupGet(d => d.Id).Returns(TrelloIds.FakeId);
 			data.SetupGet(d => d.Name).Returns("Name");
-			_clientProvider.ClientMock.SetupFor(data.Object);
+			clientProvider.ClientMock.SetupFor(data.Object);
 			TrelloConfiguration.ExpiryTime = TimeSpan.FromSeconds(1);
 
 			var board = new Board(TrelloIds.FakeId);
@@ -67,7 +71,7 @@ namespace Manatee.Trello.UnitTests
 			Thread.Sleep(1500);
 			var name2 = board.Name;
 
-			_clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Exactly(2));
+			clientProvider.ClientMock.Verify(c => c.Execute<IJsonBoard>(It.IsAny<IRestRequest>()), Times.Exactly(2));
 			TrelloConfiguration.ExpiryTime = TimeSpan.FromMilliseconds(100);
 		}
 	}
