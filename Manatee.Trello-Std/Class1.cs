ead407af8cea84f23f7f479b30c3b628ef969f02
Greg Dennis <gregsdennis@yahoo.com>
2018-01-13 23:23:33 +1300

    removed unnecessary/duplicate projects

--- Manatee.Trello-Std/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Manatee.Trello_Std
-{
-    public class Class1
-    {
-    }
-}
--- Manatee.Trello.UnitTests/Framework/MockedRestResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Manatee.Trello.Rest;
-
-namespace Manatee.Trello.UnitTests.Framework
-{
-	internal class MockedRestResponse<T> : MockRestResponse, IRestResponse<T>
-	{
-		public T Data { get; set; }
-	}
-}
