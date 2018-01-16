8dd645aeb3d1d60b8df73680a6b6a8a61d4b7d5b
Greg Dennis <gregsdennis@yahoo.com>
2018-01-16 14:08:42 +1300

    addressed manatee.json obsolete code

--- Manatee.Trello.ManateeJson/ManateeSerializer.cs
+++ Manatee.Trello.ManateeJson/ManateeSerializer.cs
@@ -18,12 +18,6 @@ namespace Manatee.Trello.ManateeJson
 		private readonly JsonSerializer _serializer;
 		private readonly MethodInfo _method;
 
-		static ManateeSerializer()
-		{
-			InitializeAbstractionMap();
-
-			JsonSerializationTypeRegistry.RegisterType(DateTimeToJson, JsonToDateTime);
-		}
 		/// <summary>
 		/// Creates and initializes a new instance of the ManateeJsonSerializer class.
 		/// </summary>
@@ -37,6 +31,8 @@ namespace Manatee.Trello.ManateeJson
 							FlagsEnumSeparator = ","
 						}
 				};
+			_serializer.CustomSerializations.RegisterType(DateTimeToJson, JsonToDateTime);
+			InitializeAbstractionMap(_serializer);
 			_method = _serializer.GetType().GetTypeInfo().DeclaredMethods
 			                     .First(m => m.Name == "Serialize")
 			                     .GetGenericMethodDefinition();
@@ -79,45 +75,45 @@ namespace Manatee.Trello.ManateeJson
 			return obj;
 		}
 
-		private static void InitializeAbstractionMap()
+		private static void InitializeAbstractionMap(JsonSerializer serializer)
 		{
-			JsonSerializationAbstractionMap.Map<IJsonAction, ManateeAction>();
-			JsonSerializationAbstractionMap.Map<IJsonActionData, ManateeActionData>();
-			JsonSerializationAbstractionMap.Map<IJsonActionOldData, ManateeActionOldData>();
-			JsonSerializationAbstractionMap.Map<IJsonAttachment, ManateeAttachment>();
-			JsonSerializationAbstractionMap.Map<IJsonBadges, ManateeBadges>();
-			JsonSerializationAbstractionMap.Map<IJsonBoard, ManateeBoard>();
-			JsonSerializationAbstractionMap.Map<IJsonBoardBackground, ManateeBoardBackground>();
-			JsonSerializationAbstractionMap.Map<IJsonBoardMembership, ManateeBoardMembership>();
-			JsonSerializationAbstractionMap.Map<IJsonBoardPersonalPreferences, ManateeBoardPersonalPreferences>();
-			JsonSerializationAbstractionMap.Map<IJsonBoardPreferences, ManateeBoardPreferences>();
-			JsonSerializationAbstractionMap.Map<IJsonBoardVisibilityRestrict, ManateeBoardVisibilityRestrict>();
-			JsonSerializationAbstractionMap.Map<IJsonCard, ManateeCard>();
-			JsonSerializationAbstractionMap.Map<IJsonCheckItem, ManateeCheckItem>();
-			JsonSerializationAbstractionMap.Map<IJsonCheckList, ManateeCheckList>();
-			JsonSerializationAbstractionMap.Map<IJsonImagePreview, ManateeImagePreview>();
-			JsonSerializationAbstractionMap.Map<IJsonLabel, ManateeLabel>();
-			JsonSerializationAbstractionMap.Map<IJsonList, ManateeList>();
-			JsonSerializationAbstractionMap.Map<IJsonMember, ManateeMember>();
-			JsonSerializationAbstractionMap.Map<IJsonMemberSearch, ManateeMemberSearch>();
-			JsonSerializationAbstractionMap.Map<IJsonMemberSession, ManateeMemberSession>();
-			JsonSerializationAbstractionMap.Map<IJsonMemberPreferences, ManateeMemberPreferences>();
-			JsonSerializationAbstractionMap.Map<IJsonNotification, ManateeNotification>();
-			JsonSerializationAbstractionMap.Map<IJsonNotificationData, ManateeNotificationData>();
-			JsonSerializationAbstractionMap.Map<IJsonNotificationOldData, ManateeNotificationOldData>();
-			JsonSerializationAbstractionMap.Map<IJsonOrganization, ManateeOrganization>();
-			JsonSerializationAbstractionMap.Map<IJsonOrganizationMembership, ManateeOrganizationMembership>();
-			JsonSerializationAbstractionMap.Map<IJsonOrganizationPreferences, ManateeOrganizationPreferences>();
-			JsonSerializationAbstractionMap.Map<IJsonParameter, ManateeParameter>();
-			JsonSerializationAbstractionMap.Map<IJsonPosition, ManateePosition>();
-			JsonSerializationAbstractionMap.Map<IJsonPowerUp, ManateePowerUp>();
-			JsonSerializationAbstractionMap.Map<IJsonPowerUpData, ManateePowerUpData>();
-			JsonSerializationAbstractionMap.Map<IJsonSearch, ManateeSearch>();
-			JsonSerializationAbstractionMap.Map<IJsonSticker, ManateeSticker>();
-			JsonSerializationAbstractionMap.Map<IJsonToken, ManateeToken>();
-			JsonSerializationAbstractionMap.Map<IJsonTokenPermission, ManateeTokenPermission>();
-			JsonSerializationAbstractionMap.Map<IJsonWebhook, ManateeWebhook>();
-			JsonSerializationAbstractionMap.Map<IJsonWebhookNotification, ManateeWebhookNotification>();
+			serializer.AbstractionMap.Map<IJsonAction, ManateeAction>();
+			serializer.AbstractionMap.Map<IJsonActionData, ManateeActionData>();
+			serializer.AbstractionMap.Map<IJsonActionOldData, ManateeActionOldData>();
+			serializer.AbstractionMap.Map<IJsonAttachment, ManateeAttachment>();
+			serializer.AbstractionMap.Map<IJsonBadges, ManateeBadges>();
+			serializer.AbstractionMap.Map<IJsonBoard, ManateeBoard>();
+			serializer.AbstractionMap.Map<IJsonBoardBackground, ManateeBoardBackground>();
+			serializer.AbstractionMap.Map<IJsonBoardMembership, ManateeBoardMembership>();
+			serializer.AbstractionMap.Map<IJsonBoardPersonalPreferences, ManateeBoardPersonalPreferences>();
+			serializer.AbstractionMap.Map<IJsonBoardPreferences, ManateeBoardPreferences>();
+			serializer.AbstractionMap.Map<IJsonBoardVisibilityRestrict, ManateeBoardVisibilityRestrict>();
+			serializer.AbstractionMap.Map<IJsonCard, ManateeCard>();
+			serializer.AbstractionMap.Map<IJsonCheckItem, ManateeCheckItem>();
+			serializer.AbstractionMap.Map<IJsonCheckList, ManateeCheckList>();
+			serializer.AbstractionMap.Map<IJsonImagePreview, ManateeImagePreview>();
+			serializer.AbstractionMap.Map<IJsonLabel, ManateeLabel>();
+			serializer.AbstractionMap.Map<IJsonList, ManateeList>();
+			serializer.AbstractionMap.Map<IJsonMember, ManateeMember>();
+			serializer.AbstractionMap.Map<IJsonMemberSearch, ManateeMemberSearch>();
+			serializer.AbstractionMap.Map<IJsonMemberSession, ManateeMemberSession>();
+			serializer.AbstractionMap.Map<IJsonMemberPreferences, ManateeMemberPreferences>();
+			serializer.AbstractionMap.Map<IJsonNotification, ManateeNotification>();
+			serializer.AbstractionMap.Map<IJsonNotificationData, ManateeNotificationData>();
+			serializer.AbstractionMap.Map<IJsonNotificationOldData, ManateeNotificationOldData>();
+			serializer.AbstractionMap.Map<IJsonOrganization, ManateeOrganization>();
+			serializer.AbstractionMap.Map<IJsonOrganizationMembership, ManateeOrganizationMembership>();
+			serializer.AbstractionMap.Map<IJsonOrganizationPreferences, ManateeOrganizationPreferences>();
+			serializer.AbstractionMap.Map<IJsonParameter, ManateeParameter>();
+			serializer.AbstractionMap.Map<IJsonPosition, ManateePosition>();
+			serializer.AbstractionMap.Map<IJsonPowerUp, ManateePowerUp>();
+			serializer.AbstractionMap.Map<IJsonPowerUpData, ManateePowerUpData>();
+			serializer.AbstractionMap.Map<IJsonSearch, ManateeSearch>();
+			serializer.AbstractionMap.Map<IJsonSticker, ManateeSticker>();
+			serializer.AbstractionMap.Map<IJsonToken, ManateeToken>();
+			serializer.AbstractionMap.Map<IJsonTokenPermission, ManateeTokenPermission>();
+			serializer.AbstractionMap.Map<IJsonWebhook, ManateeWebhook>();
+			serializer.AbstractionMap.Map<IJsonWebhookNotification, ManateeWebhookNotification>();
 		}
 
 		private static JsonValue DateTimeToJson(DateTime? date, JsonSerializer serializer)
