82f38d88124fbff34a3cc9cf3e8e6f48af6fe616
Greg Dennis <gregsdennis@yahoo.com>
2018-01-16 13:54:35 +1300

    added nuget test sln; updated build script to only build main sln

---
+++ NugetTest.Framework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manatee.Trello;
+using Manatee.Trello.ManateeJson;
+using Manatee.Trello.WebApi;
+
+namespace NugetTest.Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new ManateeSerializer();
+            TrelloConfiguration.Serializer = serializer;
+            TrelloConfiguration.Deserializer = serializer;
+            TrelloConfiguration.JsonFactory = new ManateeFactory();
+            TrelloConfiguration.RestClientProvider = new WebApiClientProvider();
+            TrelloAuthorization.Default.AppKey = "062109670e7f56b88783721892f8f66f";
+            TrelloAuthorization.Default.UserToken = "eb16c70a145d75e5ef75bdbf465ee494ac4efca148e4efcfc41060c53beb450a";
+
+            var card = new Card("ar2EsVVg");
+
+            Console.WriteLine(card);
+            Console.WriteLine(card.Description);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NugetTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Manatee.Trello;
+using Manatee.Trello.ManateeJson;
+using Manatee.Trello.WebApi;
+
+namespace NugetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new ManateeSerializer();
+            TrelloConfiguration.Serializer = serializer;
+            TrelloConfiguration.Deserializer = serializer;
+            TrelloConfiguration.JsonFactory = new ManateeFactory();
+            TrelloConfiguration.RestClientProvider = new WebApiClientProvider();
+            TrelloAuthorization.Default.AppKey = "062109670e7f56b88783721892f8f66f";
+            TrelloAuthorization.Default.UserToken = "eb16c70a145d75e5ef75bdbf465ee494ac4efca148e4efcfc41060c53beb450a";
+
+            var card = new Card("ar2EsVVg");
+
+            Console.WriteLine(card);
+            Console.WriteLine(card.Description);
+
+            Console.ReadLine();
+        }
+    }
+}
