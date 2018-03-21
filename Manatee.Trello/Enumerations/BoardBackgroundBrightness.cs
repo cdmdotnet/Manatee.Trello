691f163be92fb5b0955767512e516cb277bb44d4
Greg Dennis <gregsdennis@yahoo.com>
2018-03-22 09:47:00 +1300

    added xml comments

--- Manatee.Trello/Enumerations/BoardBackgroundBrightness.cs
+++ Manatee.Trello/Enumerations/BoardBackgroundBrightness.cs
@@ -2,11 +2,23 @@
 
 namespace Manatee.Trello
 {
+	/// <summary>
+	/// Specifies the brightness level of the board background.
+	/// </summary>
 	public enum BoardBackgroundBrightness
 	{
+		/// <summary>
+		/// Not recognized.  May have been created since the current version of this API.
+		/// </summary>
 		Unknown,
+		/// <summary>
+		/// Indicates that the board background is light.
+		/// </summary>
 		[Display(Description = "light")]
 		Light,
+		/// <summary>
+		/// Indicates that the board background is dark.
+		/// </summary>
 		[Display(Description = "dark")]
 		Dark
 	}
