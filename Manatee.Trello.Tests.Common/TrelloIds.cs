04f3137b74a77b02daebb6c2aaab277033e4ec96
Greg Dennis <gregsdennis@yahoo.com>
2018-04-18 10:36:02 +1200

    updated release notes

--- Manatee.Trello.Tests.Common/TrelloIds.cs
+++ Manatee.Trello.Tests.Common/TrelloIds.cs
@@ -8,7 +8,7 @@
 		// https://trello.com/1/authorize?key=062109670e7f56b88783721892f8f66f&name=Manatee.Trello&expiration=1day&response_type=token&scope=read,write,account
 
 		public const string AppKey = "062109670e7f56b88783721892f8f66f";
-		public const string UserToken = "4a12a76e166743e5e44f50095ccad2658924c81db64ab42a328e1e50a535f00c";
+		public const string UserToken = "ad4e5973bb9874debbec17f0a9e8df53ed454c1574f85ef2fcd22b0221e3dc88";
 		public const string UserName = "s_littlecrabsolutions";
 		public const string MemberId = "514464db3fa062da6e00254f";
 		public const string BoardId = "51478f6469fd3d9341001dae";
