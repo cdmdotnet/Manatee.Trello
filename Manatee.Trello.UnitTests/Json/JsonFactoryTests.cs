df50c13265c90d70f7616d47632daa2654c6ddc1
Greg Dennis <gregsdennis@yahoo.com>
2018-04-28 21:34:40 +1200

    json factory tests

---
+++ Manatee.Trello.UnitTests/Json/JsonFactoryTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using FluentAssertions;
+using Manatee.Trello.Json;
+using NUnit.Framework;
+
+namespace Manatee.Trello.UnitTests.Json
+{
+	[TestFixture]
+	public class JsonFactoryTests
+	{
+		public static IEnumerable JsonTypes
+		{
+			get
+			{
+				var nonEntityTypes = new[]
+					{
+						typeof(IAcceptId),
+						typeof(ISerializer),
+						typeof(IDeserializer),
+						typeof(IJsonCacheable),
+						typeof(IJsonFactory)
+					};
+				var jsonTypes = typeof(DefaultJsonFactory).Assembly
+				                                          .DefinedTypes
+				                                          .Where(t => t.Namespace == typeof(DefaultJsonFactory).Namespace &&
+				                                                      t.IsInterface)
+				                                          .Except(nonEntityTypes);
+
+				return jsonTypes.Select(t => new TestCaseData(t));
+			}
+		}
+
+		[Test]
+		[TestCaseSource(nameof(JsonTypes))]
+		public void DefaultFactoryGeneratesAllRequiredJsonTypes(Type jsonType)
+		{
+			var createMethod = typeof(DefaultJsonFactory).GetMethod(nameof(DefaultJsonFactory.Create))
+			                                             .MakeGenericMethod(jsonType);
+			var model = createMethod.Invoke(DefaultJsonFactory.Instance, new object[] { });
+
+			model.Should().NotBeNull();
+			model.Should().BeAssignableTo(jsonType);
+		}
+	}
+}
