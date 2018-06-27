fb9dea7ad5e47aa86f8c0668759932bcc5321c08
Greg Dennis <gregsdennis@yahoo.com>
2018-06-28 09:31:32 +1200

    event aggregator imported

---
+++ Manatee.Trello/Internal/IHandle.cs
@@ -0,0 +1,21 @@
+﻿namespace Manatee.Trello.Internal
+{
+	/// <summary>
+	///   A marker interface for classes that subscribe to messages.
+	/// </summary>
+	public interface IHandle {}
+
+	/// <summary>
+	///   Denotes a class which can handle a particular type of message.
+	/// </summary>
+	/// <typeparam name = "TMessage">The type of message to handle.</typeparam>
+	// ReSharper disable once TypeParameterCanBeVariant
+	public interface IHandle<TMessage> : IHandle
+	{
+		/// <summary>
+		///   Handles the message.
+		/// </summary>
+		/// <param name = "message">The message.</param>
+		void Handle(TMessage message);
+	}
+}
